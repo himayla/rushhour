d30f42d12060f929497e17e17d379812e685c6d6
Julius Kemmer <juliuskemmer@gmail.com>
2021-06-03 09:41:42 +0200

    wrote start of car object and grid object

---
+++ code/classes/car.py
@@ -0,0 +1,22 @@
+class Car():
+    def __init__(self, name, orientation, x, y, length):
+        self.name = name
+        self.orientation = orientation
+        self.x = x
+        self.y = y
+        self.length = length
+
+
+    def is_move_valid(self):
+        """
+        Gets new direction to move in (x, y).
+        checks whether car is within the playing field, checks if car is going to move correct orientation.
+        And checks the path is not blocked by other cars.
+        (maybe put all different functionalities of this function in other functions.)
+        """
+        pass
+      
+
+    def function(self):
+        pass
+        
---
+++ code/classes/grid.py
@@ -0,0 +1,24 @@
+import csv
+
+from .car import Car
+
+
+class Grid():
+    def __init__(self, source_file):
+        self.nodes = self.load_nodes(source_file)
+     
+
+    def load_cars(self, source_file):
+        """
+        load all cars into the grid.
+        """
+        cars = {}
+        with open(source_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
+
+            for row in reader:
+                cars[row['car']] = Car(row['orientation'], row['col'], row['row'], row['length'])
+
+                # rooms[room_number] = Room(room_number, room_name, long_description)
+
+        return cars
