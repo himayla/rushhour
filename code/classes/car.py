ee39c25593df89e0949de6024163ac497cb35b71
Julius Kemmer <juliuskemmer@gmail.com>
2021-06-03 10:14:10 +0200

    added starter functions with comments

--- code/classes/car.py
+++ code/classes/car.py
@@ -9,14 +9,34 @@ class Car():
 
     def is_move_valid(self):
         """
+        runs is_on_grid, is_correct_orientation and is_not_blocked to check if the move is valid.
+        if move is valid, car can move
+        """
+        pass
+
+
+    def is_on_grid(self):
+        """
         Gets new direction to move in (x, y).
-        checks whether car is within the playing field, checks if car is going to move correct orientation.
-        And checks the path is not blocked by other cars.
-        (maybe put all different functionalities of this function in other functions.)
+        checks if the position the user wants to move to is not off the grid.
+        """
+        pass
+
+    def is_correct_orientation(self):
+        """
+        Gets car orientation.
+        checks if  the move the user wants to make aligned is with the orientation of the car. 
+        i.e. car is horizontal, car can only move left and right.
+        """
+        pass
+
+    def is_not_blocked(self):
+        """
+        Gets new direction to move in (x, y).
+        checks the position the user wants to move to is not blocked by other cars.
         """
         pass
       
 
-    def function(self):
-        pass
+
         
