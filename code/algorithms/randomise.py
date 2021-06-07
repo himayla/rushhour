7d3c336c440efc6c409496bd0ad6a26310de5553
msparreboom <milasparreboom@gmail.com>
2021-06-07 16:42:16 +0200

    attempt to loop over y-axis of array

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -17,18 +17,32 @@ def random_assignment(grid):
     """
     Random
     """
+    # List with coordinates of empty spaces
     empty_spaces_coor = []
 
+    # Loop through the 2D grid to find an empty space, append to list of empty spaces
     for x in range(len(grid.coordinates)):
         for y in range(len(grid.coordinates[x])):
             if grid.coordinates[y][x] == "0":
                 empty_space = [x,y]
                 empty_spaces_coor.append(empty_space)
 
+    # Amount of empty spaces -1 to account for indexing
     total_empty = len(empty_spaces_coor) - 1
 
+    # Get coordinates for random empty space on the grid
     random_value = random.randint(0, total_empty)
     random_position = empty_spaces_coor[random_value]
+    
+    # Check for cars on x and y axis
+    empty_x = random_position[0]
+    empty_y = random_position[1]
+    print(f"random position:{random_position}")
+    for y in range(len(grid.coordinates)):
+        if grid.coordinates[x][empty_y -1] != "0":
+            print(f"value of coordinates {grid.coordinates[x][y]}")
+            print(f"actual coordinates {x,y}")
+
 
 
 
