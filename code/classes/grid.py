30d2a9de9576cacfad9d4bdaed220fa44ede1f5b
Mayla Kersten <mayla@MacBook-Pro.local>
2021-06-07 09:42:27 +0200

    worked on readability of code

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -13,7 +13,7 @@ class Grid():
 
     def load_grid(self, source_file):
         """
-        Checks title of csv file to see how big the grid is and loads the grid.
+        Plots the gameboard as a grid based on the dimensions in name of input file and fills the grid with beginning positions.
         Requires source_file. 
         """
         if "6" in source_file:
@@ -22,29 +22,30 @@ class Grid():
             size = 9   
         elif "12" in source_file:
             size = 12
+        
+        # An empty plot, to fill 
         plt.plot()
-        # loops over the 2D array and checks the value at each place
+
+        # Loops over the 2D array and checks the value at each place
         for x in range(len(self.coordinates)):
             for y in range(len(self.coordinates[x])):
-                # If the value is 0, there is no car, the square is not filled
-                if self.coordinates[y][x]=="0":
-                    rect=mpatches.Rectangle((x,size-y),1,1, 
-                                        fill = False,
-                                        color = "purple",
-                                        linewidth = 2)
-                    plt.gca().add_patch(rect)
-                    
-                # If the value is not 0, there is a car, the square is filled
-                else:
+
+                # Only fill squares if the coordinates aren't null, source https://datavizpyr.com/how-to-draw-a-rectangle-on-a-plot-in-matplotlib/
+                if self.coordinates[y][x] != "0":
                     rect=mpatches.Rectangle((x,size-y),1,1, 
                                             fill = True,
                                             color = "purple",
                                             linewidth = 2)
                     plt.gca().add_patch(rect)
+
                     # Add the name of the car on the square where it's found
                     plt.text(x + 0.25, size+0.25-y,self.coordinates[y][x],fontsize=10, color="red", weight="bold")
 
-        plt.savefig('name.png') 
+        # Save the plot as a file
+        plt.savefig('name.png')
+
+        # Pop up
+        plt.show()
      
     def load_coordinates(self, source_file):
         if "6" in source_file:
@@ -52,25 +53,31 @@ class Grid():
         elif "9" in source_file:
             size = 9   
         elif "12" in source_file:
-            size = 12   
+            size = 12
+    
         # Make a 2D array filled with 0's 
         a = [["0" for x in range(size)] for y in range(size)] 
         for car in self.cars.values():
             a[car.y-1][car.x-1]=car.name
             if car.orientation=="H":
+                
+                # If a horizontal car has length 2, draw the carname at x+1 as well
                 if car.length==2:
-                    # If a horizontal car has length 2, draw the carname at x+1 as well
                     a[car.y-1][car.x]=car.name
+                
+                # If a horizontal car has length 3, draw the carname at x+1 and x+2 as well
                 elif car.length==3:
-                    # If a horizontal car has length 3, draw the carname at x+1 and x+2 as well
                     a[car.y-1][car.x]=car.name
                     a[car.y-1][car.x+1]=car.name
+
             elif car.orientation=="V":
+
+                # If a vertical car has length 2, draw the carname at y+1 as well
                 if car.length==2:
-                    # If a vertical car has length 2, draw the carname at y+1 as well
                     a[car.y][car.x-1]=car.name
+
+                # If a vertical car has length 3, draw the carname at y+1 and y+2 as well
                 elif car.length==3:
-                    # If a vertical car has length 3, draw the carname at y+1 and y+2 as well
                     a[car.y][car.x-1]=car.name
                     a[car.y+1][car.x-1]=car.name
         return a
