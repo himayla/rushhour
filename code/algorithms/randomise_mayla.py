aafb23358b5288b593f6629d926d3d8c63c38e5d
msparreboom <milasparreboom@gmail.com>
2021-06-19 20:13:12 +0200

    shortened choose random and get possible cars in rand

--- code/algorithms/randomise_mayla.py
+++ code/algorithms/randomise_mayla.py
@@ -1,6 +1,6 @@
 import random
 
-def get_random_space(grid):
+def get_empty_spaces(grid):
     """
     Returns the coordinates a random empty space in the grid, from a list with of empty spaces in board.
     """
@@ -12,10 +12,7 @@ def get_random_space(grid):
                 empty_space = [x,y]
                 empty_spaces.append(empty_space)
 
-    random_value = random.randint(0, len(empty_spaces) - 1)
-    random_position = empty_spaces[random_value]
-
-    return random_position
+    return empty_spaces
 
 def get_relevant_rows(random_position, grid):
     """
@@ -46,84 +43,57 @@ def get_relevant_rows(random_position, grid):
     for value in x_values:
         if value != "0":
             if counter_x < random_position[0]:
-                left.append(value)                
-            right.append(value)
+                left.append(value) 
+            else: 
+                right.append(value)
             counter_x += 1 
 
     return [upper, lower, left, right]
     
-def get_random_car(upper, lower, left, right):
+def get_possible_cars(upper, lower, left, right):
     """
     Returns a random car from a all cars that could move to the empty spot.
     """
     possible = []
+    up_left = [upper, left]
+    low_right  = [lower, right]
+   
+    for direction in up_left: 
+        if direction:   
+            last_place = len(direction) -1
+            car_direction = direction[last_place]
+            count_car = 0
+            for car in direction:
+                if car == car_direction:
+                    count_car += 1
+                else:
+                    count_car = 0
+                if count_car > 1 and car_direction not in possible:
+                    possible.append(car_direction)
+    for direction in low_right: 
+        if direction:   
+            car_direction = direction[0]
+            count_car = 0
+            for car in direction:
+                if car == car_direction:
+                    count_car += 1
+                else:
+                    count_car = 0
+                if count_car > 1 and car_direction not in possible:
+                    possible.append(car_direction)
+    
+    return possible
 
-    if upper:            
-        last_place = len(upper) -1
-        upper_car = upper[last_place]
-        count_upper = 0
-        for car in upper:
-            if car == upper_car:
-                count_upper += 1
-            else:
-                count_upper = 0
-            
-            if count_upper > 1 and upper_car not in possible:
-                possible.append(upper_car)
-        
-    if lower:
-        car_left = lower[0]
-        count_lower = 0
-        for car in lower:
-            if car == car_left:
-                count_lower += 1
-            else:
-                count_lower = 0
-                        
-            if count_lower > 1 and car_left not in possible:
-                possible.append(car_left)
-
-    if left:
-        last_place = len(left) -1
-        left_car = left[last_place]
-        count_left = 0
-        for car in left:
-            if car == left_car:
-                count_left += 1
-            else:
-                count_left = 0   
-            
-            if count_left > 1 and left_car not in possible:
-                possible.append(left_car)
-
-    # If there are cars to the right of the empty space
-    if right:
-        right_car = right[0]
-        count_right = 0
-        for car in right:
-            if car == right_car:
-                count_right += 1
-            else:
-                count_right = 0
-            
-            # Add the first car to the right of the empty space to the list if it's not in there yet 
-            if count_right > 1 and right_car not in possible:
-                possible.append(right_car)
-
-    random_car = choose_random_car(possible)
-
-    return random_car
-
-def choose_random_car(possible_cars):  
+def choose_random(possibilities):  
     """
     Returns a random car from the list of possible cars. 
     """  
-    if possible_cars:
-        random_car = random.choice(possible_cars)
+    if possibilities:
+        possibility = random.choice(possibilities)
     else:
-        random_car = ""
+        possibility = ""
 
-    return random_car
+    return possibility
 
 def move_car(grid, position, random_car, upper, lower, left, right):
     """
@@ -208,8 +178,11 @@ def rearrange_board(grid):
     Rearranges the board by finding an empty space and the different directions
     """
     # Choose a random space out of the empty spaces
-    position = get_random_space(grid.board)
-
+    # for line in grid.board: 
+    #     print(line)
+    empty_spaces = get_empty_spaces(grid.board)
+    position = choose_random(empty_spaces)
+    # print(f"empty space: {position}")
     # Create a list for all the X values and Y values connected to this empty space
     directions = get_relevant_rows(position, grid.board)
     
@@ -219,8 +192,9 @@ def rearrange_board(grid):
     right = directions[3]
 
     # Create a list with the cars left, up, right, down to the empty space and Choose random car from this list that can move to the empty space
-    random_car = get_random_car(upper, lower, left, right)
-
+    possible_cars = get_possible_cars(upper, lower, left, right)
+    random_car = choose_random(possible_cars)
+    # print(f"car: {random_car}")
     if random_car != "":
 
         # Add the new move to the list of moves
@@ -233,9 +207,13 @@ def rearrange_board(grid):
 def random_solver(grid):
     """
     Solve the game by looking for empty spots, until car X (red car) is in the winning position.
-    """    
+    """
+    count = 0    
     while grid.solution not in grid.list_of_moves:
         rearrange_board(grid)
+        count += 1
+        # if count == 20:
+        #     break
 
     # Print final results
     print(f"Final board:")
