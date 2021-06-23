a50ffd246f8a0c5d41fbb7f7e70a898e07e25dac
msparreboom <milasparreboom@gmail.com>
2021-06-23 16:37:01 +0200

    added comments to hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -1,24 +1,12 @@
 """
-pseudocode: 
-input is path
-count = 0
-1. Get a random board from the path that's at least 25 moves away from the final board. 
-2. Choose another board between 15 and 25 further down the path. 
-3. Do a breath first algorithms on the first chosen board and save the steps in a dictionary. 
-4. When the final board is found:
-5. If the path to the final board is smaller than the path that was given originally to the algorithm. 
-    6. Put the moves of the breadth first algorithm into the place of the original path
-    count += 1
-7. Else:
-    8. Repeat with another combination of boards. 
-    count = 0
-9. if count = 100 / 1000 / 100000:
-    return path
+Hillclimber algorithm doesn't produce a solution in a certain amount of moves like the other algorithms. 
+The hillclimber uses the chosen path from other algorithms such as depth first or randomise, then tries to improve that path. 
+The algorithm does that by selecting a part of the path and running a breadth first on that part to see if it is possible to shorten it. 
+It will do that for as many tries as is specified in the main.py. Then the shortest path is returned to main.py
 """
 from code.classes.model import Model
 from .breadth_first import Breadthfirst
 import random
-import copy
 
 class HillClimber(Breadthfirst):
     def __init__(self, path):
@@ -34,6 +22,9 @@ class HillClimber(Breadthfirst):
         self.moves = []
 
     def determine_path(self, path):
+        """
+        Determine path chooses random subset of the path to run a breadth first algorithm on. 
+        """
         length = len(path)
         if length > 80:
             self.path_check = random.randint(60, 80)
@@ -44,6 +35,10 @@ class HillClimber(Breadthfirst):
         self.end_board = path[self.start_value + self.path_check - 1]
 
     def check_path(self, final_model):
+        """
+        check path checks whether the newly found path is shorter than the original path. 
+        If it is shorter, the relevant section of the path is replaced by the shorter path, if not, it's not. 
+        """
         alt_path = self.find_solution_seq(final_model)
         
         if len(alt_path) < self.path_check:
@@ -59,12 +54,21 @@ class HillClimber(Breadthfirst):
             return False
 
     def check_solution(self, board):
+        """
+        check solution checks to see whether the breadth first algorithm has reached the chosen end board of the subset of the path
+        """
         if board == self.end_board:
             return True
         else:
             return False
 
     def run(self, iterations):
+        """
+        for the number of iterations specified in main.py, the hillclimber will choose a random subset of the path. 
+        On that path a breadth first is run until the end board is found. 
+        Then the new path to the end board will be checked to see if it is shorter, and if so, replaces the original subset of the path. 
+        After all the iterations are completed, the shortest found path is returned to the user in main.py
+        """
         for i in range(iterations):
             self.determine_path(self.path)
             new_model = self.start_board
@@ -74,7 +78,7 @@ class HillClimber(Breadthfirst):
                     break
                 else:
                     self.build_children(new_model)
-                if len(self.states) ==1:
+                if len(self.states) == 1:
                     break
                 new_model = self.states.pop(1)
         return self.path
